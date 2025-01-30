e36c5a9750f7e49ea4b1e039939ea393435297b3
chaitanya7517 <chaitanyanalawade84@gmail.com>
2025-01-31 02:01:59 +0530

    Added Team Names for batter readability

--- scripts/b2c/test.js
+++ scripts/b2c/test.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://github.com/');
+  await page.goto('https://drxuat.bajajfinservhealth.in/login');
 });
